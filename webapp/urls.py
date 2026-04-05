07a19fcfcf43596130acd5e60420eb8a20f7362a
Nuredin Seid <manmush09@gmail.com>
2026-04-05 21:53:12 +0300

    modifing

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    # path('',views.home,name=''),
+    path('',views.home,name=''),
     path('register',views.register,name='register'),
     path('my-login',views.my_login,name='my-login'),
     path('my-logout',views.my_logout,name='my-logout'),
